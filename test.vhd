4ad91d85e1f9cf9c7d45c6aa76096f572a0c6c28
walid885 <bettaieb88walid@gmail.com>
2025-04-09 20:27:39 +0200

    setting up the hirearchy of the different parts

---
+++ test.vhd
@@ -0,0 +1,16 @@
+
+entity T01_HelloWorldTb is
+end entity;
+  
+architecture sim of T01_HelloWorldTb is
+begin
+  
+    process is
+    begin
+  
+        report "Hello World!";
+        wait;
+  
+    end process;
+  
+end architecture;
